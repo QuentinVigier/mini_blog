c161fc1f47be122940b73329a2d6bc098e84651c
QuentinVigier <quentin.vigier12@gmail.com>
2024-09-26 14:18:10 +0200

    AJout des routes dans le controller de Article + Création des templates pour la page avec tout les articles et la page des détails d'un article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+#[Route('/articles')] 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    #[Route('/', name: 'app_article_all', methods: ['GET'])]
+    public function index(ArticleRepository $ar): Response
     {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+        $allArticles = $ar->findAll();
+        return $this->render('article/all.html.twig', [
+            'allArticles' => $allArticles,
+        ]);
+    }
+
+    #[Route('/{title}', name: 'app_article_show', methods: ['GET'])]
+    public function show(string $title, ArticleRepository $ar): Response
+    {
+        $article = $ar->findOneBy(['title' => $title]);
+
+        if (!$article) {
+            throw $this->createNotFoundException('L\'article demandé n\'existe pas.');
+        }
+
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
         ]);
     }
 }
